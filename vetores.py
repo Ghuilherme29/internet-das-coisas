e534e83f86b9dc3ea7124e46802726c5de94b304
Ghuilherme29 <guilherme.silva15@ba.estudante.senai.br>
2026-08-18 16:30:19 -0300

    Add files via upload

---
+++ vetores.py
@@ -0,0 +1,11 @@
+numeros = [33,11,54,43,12,43]
+nomes = ["Carlos", "Anderson", "Isabele"]
+
+numeros.pop(2)
+print(numeros)
+
+
+#append - Insere um novo vetor, sempre na última casa
+#Insert - Insere um novo valor na posição desejada
+#pop - deleta um valor pela sua posição
+#remove - deleta um valor pelo seu conteudo
