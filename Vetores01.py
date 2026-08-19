68b6c93191c17a17398c9e235b92836a495fc696
Ghuilherme29 <guilherme.silva15@ba.estudante.senai.br>
2026-08-19 16:31:31 -0300

    Add files via upload

---
+++ Vetores01.py
@@ -0,0 +1,13 @@
+numeros = [33,12,45,76,23,33]
+nomes = ["Julia", "Maria", "Luiza"]
+
+maior = max(numeros)
+minor = min(numeros)
+print(f"Maior:{maior}|| Minor: {minor}")
+
+#Sort - Ordena o vetor.
+#reverse - inverte as posições do vetor.
+#len - Informar quantos valores exstem dentro de um vetor.
+#count - informa a quantidade de um valor especifico.
+#sum - soma todos os valores de um vetor.
+# Max - min 
